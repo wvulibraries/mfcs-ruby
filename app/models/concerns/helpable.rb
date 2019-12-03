3191dc77b85b2dc42e71451ed2a6c83e5cbbff68
ddavisgraphics <dave@ddavisgraphics.com>
2019-12-03 10:46:58 -0500

    Mitigating dead files.

--- app/models/concerns/helpable.rb
+++
@@ -1,7 +0,0 @@
-# adds a helpable module to reuse in all fields
-module Helpable
-  extend ActiveSupport::Concern
-  included do
-    enum help_type: { no_help: 0, plain_text: 1, html_text: 2, web_url: 3 }
-  end
-end
