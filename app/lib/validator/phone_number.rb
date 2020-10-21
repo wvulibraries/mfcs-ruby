8f9adcd840ca1c8fa19530af8d372913a43f0904
ddavisgraphics <dave@ddavisgraphics.com>
2020-10-21 17:58:53 -0400

    Phone Number Validations

---
+++ app/lib/validator/phone_number.rb
@@ -0,0 +1,19 @@
+# Validates that a given input matches a phone number
+class Validator::PhoneNumber
+  # Basic constructor for all Validator Methods
+  # @author David J. Davis
+  def initialize(input, field_info, form_id)
+    @field_info = field_info
+    @input = input
+    @form_id = form_id
+  end
+
+  # Peform function is in all validator classes and returns
+  # make the actual validation check to return a boolean.
+  #
+  # @author David J. Davis
+  # @return [Boolean]
+  def perform
+    @input.match?(/\A1?[-. ]?(\(\d{3}\)?[-. ]?|\d{3}?[-. ]?)?\d{3}?[-. ]?\d{4}\z/)
+  end
+end
---
+++ spec/lib/validator/phone_number_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Validator::PhoneNumber, type: :model do
+  # shared examples
+  context 'base behaviors' do
+    it_behaves_like 'a validator object' 
+  end
+  
+  context '.perform' do
+    valid_numbers = [
+      '(333)111-2385',
+      '333-111-2385',
+      '333.111.2385',
+      '333 111 2385',
+      '(333) 111-2385',
+      '(333).111.2385',
+      '(333) 111 2385',
+      '(333)111.2385',
+      '1-230-234-2356'
+    ]
+
+    valid_numbers.each do |n|
+      it "valid phone number -- #{n}" do
+        dv = described_class.new(n, {}, 1)
+        expect(dv.perform).to be true
+      end
+    end
+
+    invalid_numbers = [
+      '123.234.123',
+      'joe',
+      '123-234-1234x',
+      '1-230-234-2356x'
+    ]
+    
+    invalid_numbers.each do |n|
+      it "invalid phone number -- #{n}" do
+        dv = described_class.new(n, {}, 1)
+        expect(dv.perform).to be false
+      end
+    end
+  end
+end 
