1b0fbe29dee99e5a2d882348f8111aa636e8fd1d
ddavisgraphics <dave@ddavisgraphics.com>
2020-10-21 14:48:10 -0400

    Alpha Done, Cleaned and Tested.

---
+++ app/lib/validator/alpha_numeric.rb
@@ -0,0 +1,19 @@
+# Validates a variety of alpha numerical characters
+class Validator::AlphaNumeric
+  # Basic constructor for all Validator Methods
+  # @author David J. Davis
+  def initialize(input, field_info, form_id)
+    @field_info = field_info
+    @input = input
+    @form_id = form_id
+  end
+
+  # Peform function is in all validator classes and returns
+  # make the actual validation check to return a boolean.
+  #
+  # @author David J. Davis
+  # @return [Boolean]
+  def perform
+    @input.match?(/\A[a-z0-9]+\Z/i)
+  end
+end
