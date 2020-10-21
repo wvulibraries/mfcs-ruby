3cc8dc980724a706f5972e1f2aa517a29c4588b6
ddavisgraphics <dave@ddavisgraphics.com>
2020-10-21 15:04:09 -0400

    Alpha Complete and Tested.

---
+++ app/lib/validator/alpha.rb
@@ -0,0 +1,19 @@
+# Validator Class that checks for Alphabetical characters only.
+class Validator::Alpha
+  # Basic constructor for all Validator Methods
+  # @author David J. Davis
+  def initialize(input, field_info, form_id)
+    @field_info = field_info
+    @input = input
+    @form_id = form_id
+  end
+
+  # Peform function is in all validator classes and returns
+  # make the actual validation check to return a boolean.
+  #
+  # @author David J. Davis
+  # @return [Boolean]
+  def perform
+    @input.match?(/\A[a-z]+\z/i)
+  end
+end
