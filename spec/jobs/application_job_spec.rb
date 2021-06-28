de4255ccc3a50a3ad80333a9d75332f913ab4c57
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-06-28 15:52:55 -0400

    updated application job - incorrect job set

--- spec/jobs/application_job_spec.rb
+++ spec/jobs/application_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ConvertingFileJob, type: :job do
+RSpec.describe ApplicationJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
