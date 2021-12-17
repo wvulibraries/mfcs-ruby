59ee10c1f3b88ae870dc4038f32ef9a728be254a
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-12-17 15:08:51 -0500

    rubocop automatic formatting

--- app/jobs/working_file_job.rb
+++ app/jobs/working_file_job.rb
@@ -7,20 +7,23 @@ class WorkingFileJob < ApplicationJob
     item = Item.find(media.item_id)
 
     FileUtils.mkdir_p(item.working_path) unless File.directory?(item.working_path)
-    FileUtils.cp(media.path, item.working_path) unless File.exists?(item.working_path.join(media.filename))
+    unless File.exist?(item.working_path.join(media.filename))
+      FileUtils.cp(media.path,
+                   item.working_path)
+    end
 
     # hash = {source_path: media.path, destination_path: item.working_path}, filename: media.filename}
     # FileCopyJob.perform_later(hash.to_json)
 
-    working_file = Media.new(media.attributes.merge(id: nil, media_type: :working, path: item.working_path.join(media.filename)))
+    working_file = Media.new(media.attributes.merge(id: nil, media_type: :working,
+                                                    path: item.working_path.join(media.filename)))
     working_file.save
 
     # do we need so save the media object back to the item?
     # item[:data][media.fieldname] << working_file
     # item.save
 
-    # ConvertingFileJob.perform_later(working_file.id)
+    ConvertingFileJob.perform_later(working_file.id)
     # ProcessingJob.perform_later(working_file.id)
   end
-
 end
