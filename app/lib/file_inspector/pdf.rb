4f1bde3278b28a2de3f65efb26c249c5e82a8660
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-12-20 14:47:40 -0500

    rubocop automatic updates

--- app/lib/file_inspector/pdf.rb
+++ app/lib/file_inspector/pdf.rb
@@ -1,5 +1,6 @@
 class FileInspector::Pdf
   def self.matches?(mime)
-    %w[application text].include?(mime.split('/')[0].downcase) && mime.split('/')[1].downcase.include?('pdf') 
+    %w[application
+       text].include?(mime.split('/')[0].downcase) && mime.split('/')[1].downcase.include?('pdf')
   end
 end
