42dcbfb4f43a023f9b39fff6baf9315f551fdfe0
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-12-21 13:15:57 -0500

    rubocop automatic updates

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,19 +1,17 @@
 # Media Controller Class
 class MediaController < ApplicationController
-    ## gets image and shows image
-    def index
-      media = Media.where(id: params[:id]).first
-      send_file(media.path, { :type => media.mime_type, :disposition => 'inline' })
-    end
-  
-    ## gets image and shows image
-    def thumb
-      begin
-        media = Media.where(id: params[:id], media_type: 'thumbnail').first
-        send_file(media.path, { :type => media.mime_type, :disposition => 'inline' })
-      rescue
-        send_file('app/frontend/packs/images/no-image.png', { :type => 'image/png', :disposition => 'inline' })
-      end
-    end
+  ## gets image and shows image
+  def index
+    media = Media.where(id: params[:id]).first
+    send_file(media.path, { type: media.mime_type, disposition: 'inline' })
+  end
 
+  ## gets image and shows image
+  def thumb
+    media = Media.where(id: params[:id], media_type: 'thumbnail').first
+    send_file(media.path, { type: media.mime_type, disposition: 'inline' })
+  rescue StandardError
+    send_file('app/frontend/packs/images/no-image.png',
+              { type: 'image/png', disposition: 'inline' })
+  end
 end
