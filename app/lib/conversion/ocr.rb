b69036638a9a8034c348ace1a5c6317874c8b832
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-12-20 22:40:34 -0500

    rubocop automatic updates

--- app/lib/conversion/ocr.rb
+++ app/lib/conversion/ocr.rb
@@ -1,21 +1,19 @@
-class Conversion::Ocr 
+class Conversion::Ocr
   def initialize(media_id)
-    @media = Media.find(media_id) 
-  end  
+    @media = Media.find(media_id)
+  end
 
-  def ocr(filepath, language = 'eng')
-    ocr_file = RTesseract.new filepath.to_s 
-    ocr_file.to_s 
-  end 
+  def ocr(filepath, _language = 'eng')
+    ocr_file = RTesseract.new filepath.to_s
+    ocr_file.to_s
+  end
 
   def perform
     @media['ocr_text'] = ocr(@media.path)
     @media.save
-  end 
-
-  def save_text 
   end
-  
-  def save_pdf 
-  end 
-end 
+
+  def save_text; end
+
+  def save_pdf; end
+end
