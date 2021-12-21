f8672c41be195255586eeba21af5871ef8495158
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-12-21 11:17:07 -0500

    rubocop automatic updates

--- app/jobs/process_file_job.rb
+++ app/jobs/process_file_job.rb
@@ -12,54 +12,53 @@ class ProcessFileJob < ApplicationJob
     # for some files so they are not in the correct
     # location
     file_path = find_file(file['path'], original_filename)
+    return unless !file_path.nil? && File.exist?(file_path)
+    
+    # queue up jobs to copy archive and working copies
+    # hash = {source_path: file_path, destination_path: item.archival_path, filename: original_filename}
+    # FileCopyJob.perform_later(hash.to_json)
+    # hash[:destination_path] = item.working_path
+    # FileCopyJob.perform_later(hash.to_json)
 
-    if !file_path.nil? && File.exist?(file_path)
-      # queue up jobs to copy archive and working copies
-      # hash = {source_path: file_path, destination_path: item.archival_path, filename: original_filename}
-      # FileCopyJob.perform_later(hash.to_json)
-      # hash[:destination_path] = item.working_path
-      # FileCopyJob.perform_later(hash.to_json)
+    # copy file for archive and working copies
+    copy_file(file_path, item.archival_path, original_filename)
+    # self.copy_file(file_path, item.working_path, original_filename)
 
-      # copy file for archive and working copies
-      copy_file(file_path, item.archival_path, original_filename)
-      # self.copy_file(file_path, item.working_path, original_filename)
+    # full path to the file
+    archive_file_path = item.archival_path.join(original_filename)
 
-      # full path to the file
-      archive_file_path = item.archival_path.join(original_filename)
+    hash_values = {
+      item_id: item.id,
+      form_id: item.form_id,
+      media_type: :archive,
+      filename: original_filename,
+      path: archive_file_path,
+      fieldname: field_name
+    }
 
-      hash_values = {
-        item_id: item.id,
-        form_id: item.form_id,
-        media_type: :archive,
-        filename: original_filename,
-        path: archive_file_path,
-        fieldname: field_name
-      }
+    # creates archive media object in database
+    archive_media = Media.new(hash_values)
+    archive_media.save
 
-      # creates archive media object in database
-      archive_media = Media.new(hash_values)
-      archive_media.save
+    # add archive media to item
+    item[:data][field_name] << archive_media
 
-      # add archive media to item
-      item[:data][field_name] << archive_media
+    # save updated item
+    item.record_timestamps = false
+    item.save
 
-      # save updated item
-      item.record_timestamps = false
-      item.save
+    WorkingFileJob.perform_later(archive_media.id)
 
-      WorkingFileJob.perform_later(archive_media.id)
+    # update hash_values for working copy
+    # hash_values[:media_type] = :working
+    # hash_values[:path] = item.working_path.join(original_filename)
 
-      # update hash_values for working copy
-      # hash_values[:media_type] = :working
-      # hash_values[:path] = item.working_path.join(original_filename)
+    # creates working media object in database
+    # working_media = Media.new(hash_values)
+    # working_media.save
 
-      # creates working media object in database
-      # working_media = Media.new(hash_values)
-      # working_media.save
-
-      # convert media from working copy
-      # ConvertingFileJob.perform_later(working_media.id)
-    end
+    # convert media from working copy
+    # ConvertingFileJob.perform_later(working_media.id)
   end
 
   private
