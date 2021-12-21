81fd140f0d7673e89442f1730433126edaa5d48d
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-12-21 11:03:28 -0500

    rubocop recommened changes

--- app/jobs/file_copy_job.rb
+++ app/jobs/file_copy_job.rb
@@ -7,13 +7,13 @@ class FileCopyJob < ApplicationJob
     source_path = hash_values['source_path']
     destination_path = hash_values['destination_path']
     filename = hash_values['filename']
-    
-    # create the path if it doesn't exist 
+
+    # create the path if it doesn't exist
     FileUtils.mkdir_p(destination_path) unless File.directory?(destination_path)
 
     # creates path to the new file
-    file_path = destination_path + '/' + filename 
+    file_path = destination_path + '/' + filename
 
-    FileUtils.cp(source_path, file_path)   
+    FileUtils.cp(source_path, file_path)
   end
 end
--- app/jobs/import_job.rb
+++ app/jobs/import_job.rb
@@ -5,38 +5,38 @@ class ImportJob < ApplicationJob
     @hash = JSON.parse(File.read(file))
 
     # only import objects
-    if @hash.present? && Item.where(id: @hash['ID']).count == 0
-      # rename timestamps and convert format
-      @hash['created_at'] = Time.at(@hash.delete('createTime').to_f) if @hash['createTime']
-      @hash['updated_at'] = Time.at(@hash.delete('modifiedTime').to_f) if @hash['modifiedTime']
+    return unless @hash.present? && Item.where(id: @hash['ID']).count.zero?
 
-      # delete items
-      @hash.delete('parentID')
-      @hash.delete('defaultProject')
-      # @hash.delete('modifiedTime')
-      # @hash.delete('createTime')
-      # @hash.delete('modifiedBy')
-      # @hash.delete('createdBy')
-      @hash.delete('objectProject')
+    # rename timestamps and convert format
+    @hash['created_at'] = Time.at.getlocal(@hash.delete('createTime').to_f) if @hash['createTime']
+    @hash['updated_at'] = Time.at.getlocal(@hash.delete('modifiedTime').to_f) if @hash['modifiedTime']
 
-      # save old file info
-      files_hash = {}
-      @hash['data'].each do |key, value|
-        if field_has_files?(value)
-          files_hash[key] = value
-          @hash['data'].delete(key)
-        end
+    # delete items
+    @hash.delete('parentID')
+    @hash.delete('defaultProject')
+    # @hash.delete('modifiedTime')
+    # @hash.delete('createTime')
+    # @hash.delete('modifiedBy')
+    # @hash.delete('createdBy')
+    @hash.delete('objectProject')
+
+    # save old file info
+    files_hash = {}
+    @hash['data'].each do |key, value|
+      if field_has_files?(value)
+        files_hash[key] = value
+        @hash['data'].delete(key)
       end
-
-      array = @hash.keys
-      array.each { |key| update_key(key) }
-
-      # create items
-      create_item(@hash)
-
-      # queue job to process files
-      ProcessFilesJob.perform_later(@hash['id'], files_hash) unless files_hash.empty?
     end
+
+    array = @hash.keys
+    array.each { |key| update_key(key) }
+
+    # create items
+    create_item(@hash)
+
+    # queue job to process files
+    ProcessFilesJob.perform_later(@hash['id'], files_hash) unless files_hash.empty?
   end
 
   private
