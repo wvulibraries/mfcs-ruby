9e50364812a60d99f1d6598d4ed253099a475662
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2021-12-21 12:58:56 -0500

    Rubocop Automatic Updates

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,7 @@
 class ProjectsController < ApplicationController
-
   # GET /projects
   def index
     breadcrumb 'Select Project', '/projects', title: 'List Objects - Select Project', match: :exact
     @projects = Project.all.order(:name)
   end
-
 end
