4e2f67fdb3b46b86a8d2d8ce6fb822450e61fc75
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-01-04 12:13:29 -0500

    rubocop recommened changes

--- app/lib/ffmpeg/video/metadata.rb
+++ app/lib/ffmpeg/video/metadata.rb
@@ -4,7 +4,8 @@ class FFMPEG::Video::Metadata
   end
 
   def fetch
-    stdout, stderr, status = Open3.capture3("#{ffprobe} -v error -select_streams v:0 -show_entries stream=width,height,duration,bit_rate -print_format json #{@file}")
+    ffmpeg_params = "-v error -select_streams v:0 -show_entries stream=width,height,duration,bit_rate -print_format json"
+    stdout, _stderr, _status = Open3.capture3("#{ffprobe} #{ffmpeg_params} #{@file}")
     JSON.parse(stdout)['streams'][0].with_indifferent_access
   end
 
